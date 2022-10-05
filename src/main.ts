f9d82db61554a0a7dade0b26ae186799ee715628
Byron <byronogis@outlook.com>
2022-10-06 00:17:52 +0800

    chore: add router with file system based

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router from '~/router'
 import './style.css'
 import App from './App.vue'
 
@@ -9,4 +10,5 @@ const app = createApp(App)
 
 app
 .use(pinia)
+.use(router)
 .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// File system based route
+// See https://github.com/hannoeru/vite-plugin-pages
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      Pages(),
+    ],
     resolve: {
       alias: {
         '~': '/src',

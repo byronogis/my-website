e905614fb2636102b137e693c67a4fefb83ae69b
Byron <byronogis@outlook.com>
2022-10-06 11:15:20 +0800

    chore: remove unused and lint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
+import App from './App.vue'
 import pinia from '~/store'
 import router from '~/router'
 import './style.css'
-import App from './App.vue'
 
 const app = createApp(App)
 
 app
-.use(pinia)
-.use(router)
-.mount('#app')
+  .use(pinia)
+  .use(router)
+  .mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -8,7 +8,7 @@ export default pinia
 export const useStore = defineStore('main', () => {
   const count = ref(0)
   const increment = () => {
-    count.value ++
+    count.value++
   }
   return {
     count,

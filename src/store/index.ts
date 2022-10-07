f1074d36b37a5f4acbf40b83ca1b963f424f3c7a
Byron <byronogis@outlook.com>
2022-10-07 17:19:07 +0800

    chore: clear

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,5 @@
-import { createPinia, defineStore } from 'pinia'
+import { defineStore } from 'pinia'
 import { ref } from 'vue'
-
-const pinia = createPinia()
-
-export default pinia
 
 export const useStore = defineStore('main', () => {
   const count = ref(0)

c7bdc5ca5d12ca988b24c14ac35d2b680c5b9c77
Byron <byronogis@outlook.com>
2022-10-07 18:49:43 +0800

    chore: use auto import

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import Markdown from 'vite-plugin-vue-markdown'
 // See https://github.com/antfu/unplugin-vue-components
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+// Auto import APIs
+// See https://github.com/antfu/unplugin-auto-import#readme
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command: _commond, mode: _mode }) => {
@@ -21,6 +24,16 @@ export default defineConfig(({ command: _commond, mode: _mode }) => {
       Components({
         resolvers: [
           NaiveUiResolver(),
+        ],
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+        ],
+        dts: './src/auto-imports.d.ts',
+        dirs: [
+          './src/store',
         ],
       }),
     ],

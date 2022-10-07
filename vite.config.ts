3e0f717c1f679d9fdf8aa45fbdb8c12da0222417
Byron <byronogis@outlook.com>
2022-10-07 18:02:23 +0800

    chore: use md plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import vue from '@vitejs/plugin-vue'
 // File system based route
 // See https://github.com/hannoeru/vite-plugin-pages
 import Pages from 'vite-plugin-pages'
+import Markdown from 'vite-plugin-vue-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command: _commond, mode: _mode }) => {
   return {
     plugins: [
-      vue(),
+      vue({
+        include: [/\.vue$/, /\.md$/],
+      }),
       Pages(),
+      Markdown(),
     ],
     resolve: {
       alias: {

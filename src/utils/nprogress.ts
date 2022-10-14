82b633627c3070677c5428d678cb288290aebb93
Byron <byronogis@outlook.com>
2022-10-14 12:44:19 +0800

    chore: adjust nprogress config

--- src/utils/nprogress.ts
+++ src/utils/nprogress.ts
@@ -4,13 +4,14 @@ import 'nprogress/nprogress.css'
 
 // 全局进度条的配置
 // See https://github.com/rstacruz/nprogress#configuration
-// NProgress.configure({
-//   easing: 'ease', // 动画方式
-//   speed: 1000, // 递增进度条的速度
-//   showSpinner: false, // 是否显示加载ico
-//   trickleSpeed: 200, // 自动递增间隔
-//   minimum: 0.3, // 初始化时的最小百分比
-// })
+NProgress.configure({
+  // easing: 'ease', // 动画方式
+  // speed: 1000, // 递增进度条的速度
+  // showSpinner: false, // 是否显示加载ico
+  // trickleSpeed: 200, // 自动递增间隔
+  // minimum: 0.3, // 初始化时的最小百分比
+  showSpinner: false,
+})
 
 export default {
   // 打开进度条

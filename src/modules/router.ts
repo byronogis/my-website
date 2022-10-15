70b2f048dc2009e64b8d50111d5a8818100c3a03
Byron <byronogis@outlook.com>
2022-10-15 08:27:51 +0800

    chore: choose webhistory of router

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
 
 const routes = setupLayouts(generatedRoutes)
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 

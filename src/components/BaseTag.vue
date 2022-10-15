b6521a04c95a48afc1b950e3392b300e14876465
Byron <byronogis@outlook.com>
2022-10-15 12:03:37 +0800

    feat: let tag clickable

--- src/components/BaseTag.vue
+++ src/components/BaseTag.vue
@@ -2,6 +2,16 @@
 const props = defineProps<{
   data: string[]
 }>()
+
+const router = useRouter()
+const openEssaysOfTag = (tag: string) => {
+  router.push({
+    path: 'essays',
+    query: {
+      tag,
+    },
+  })
+}
 </script>
 
 <template>
@@ -10,6 +20,7 @@ const props = defineProps<{
       v-for="tag in props.data" :key="tag"
       inline-block mx-2 px-2
       bg-gray-100
+      @click="openEssaysOfTag(tag)"
     >
       {{ tag }}
     </span>

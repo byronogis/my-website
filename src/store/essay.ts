38e8e30ca9973e8e85d66abc1573a31a58226bdc
Byron <byronogis@outlook.com>
2022-10-15 11:12:00 +0800

    chore: empty data handle

--- src/store/essay.ts
+++ src/store/essay.ts
@@ -3,9 +3,14 @@ import type { EssayFrontmatter } from '~/types'
 
 export const useEssayStore = defineStore('essay', () => {
   const essays = import.meta.glob('~/docs/essays/**/*.md', { import: 'frontmatter', eager: true }) as Record<string, EssayFrontmatter>
+  // 处理 md 可能的异常
   Object.keys(essays).forEach((val) => {
     // essays[val]._path = val
     essays[val]._name = val.split('/').at(-1)?.split('.md')[0]
+    essays[val].title = essays[val].title || '无标题'
+    essays[val].desc = essays[val].desc || '无简述'
+    essays[val].date = essays[val].date || '1999-12-31 23:59'
+    essays[val].tags = Array.isArray(essays[val].tags) ? essays[val].tags : ['无标签']
   })
 
   //

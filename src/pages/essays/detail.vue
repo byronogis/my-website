ac028bdc96f0d236c79d2fd739a3b99634da4515
Byron <byronogis@outlook.com>
2022-10-15 14:12:18 +0800

    style: essay detail

--- src/pages/essays/detail.vue
+++ src/pages/essays/detail.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 const route = useRoute()
 
-const title = route.query.title
-
-const name = shallowRef()
+const component = shallowRef()
 const frontmatter = ref()
 
-const md = () => import(`../../docs/essays/${title}.md`)
+const md = () => import(`../../docs/essays/${route.query.title}.md`)
 md().then((res) => {
-  name.value = res.default
+  component.value = res.default
   frontmatter.value = res.frontmatter
 })
 </script>
@@ -18,11 +16,24 @@ md().then((res) => {
     <h1
       class="ped-title"
       pt-4 pb-8
+      sticky top-15 z-9
     >
-      {{ frontmatter.title }}
+      {{ frontmatter?.title }}
     </h1>
+    <div
+      class="ped-info"
+      pb-4
+      grid grid-cols-2
+    >
+      <BaseTag :data="frontmatter?.tags || []" />
+      <div text-14px>
+        时间:
+        <span px-1>{{ frontmatter?.date }}</span>
+      </div>
+    </div>
+    <BaseDivider />
     <component
-      :is="name"
+      :is="component"
       v-highlight
     />
   </div>
@@ -30,7 +41,8 @@ md().then((res) => {
 
 <style lang="postcss">
 .ped-title {
-  color: var(--liam-primary)
+  color: var(--liam-primary);
+  background-color: var(--liam-bg);
 }
 </style>
 

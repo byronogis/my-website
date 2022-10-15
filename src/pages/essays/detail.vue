0320b3857577f0425a83c978c99efa837b4d705c
Byron <byronogis@outlook.com>
2022-10-15 13:09:07 +0800

    chore: add md title in essay detail

--- src/pages/essays/detail.vue
+++ src/pages/essays/detail.vue
@@ -4,19 +4,35 @@ const route = useRoute()
 const title = route.query.title
 
 const name = shallowRef()
+const frontmatter = ref()
 
 const md = () => import(`../../docs/essays/${title}.md`)
 md().then((res) => {
   name.value = res.default
+  frontmatter.value = res.frontmatter
 })
 </script>
 
 <template>
-  <component
-    :is="name"
-    v-highlight
-  />
+  <div class="pages-essays-detail">
+    <h1
+      class="ped-title"
+      pt-4 pb-8
+    >
+      {{ frontmatter.title }}
+    </h1>
+    <component
+      :is="name"
+      v-highlight
+    />
+  </div>
 </template>
+
+<style lang="postcss">
+.ped-title {
+  color: var(--liam-primary)
+}
+</style>
 
 <route lang="yaml">
 name: essays-title

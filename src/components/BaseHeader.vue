c1b3b08e53e5078a1b1086a5e5bf508da1723d60
Byron <byronogis@outlook.com>
2022-10-24 15:05:35 +0800

    style: make current nav highlight

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -6,6 +6,14 @@ const props = defineProps<{
 
 const windowObj = window
 const router = useRouter()
+const route = useRoute()
+
+const matchPath = (path: string) => {
+  if (path === '/')
+    return route.path === path
+  else
+    return route.path.startsWith(path)
+}
 </script>
 
 <template>
@@ -31,9 +39,17 @@ const router = useRouter()
         v-for="{ text, path, outer = false } in $props.navs"
         :key="path"
         pl-8 cursor-pointer
+        relative
+        :style="{ color: matchPath(path) ? 'var(--liam-primary)' : undefined }"
         @click="!outer ? router.push(path) : windowObj.open(path, '_blank')"
       >
         {{ text }}
+        <span
+          v-if="outer"
+          absolute top--1 right--1
+          w-2 h-2
+          i-carbon-arrow-up-right
+        />
       </span>
     </div>
   </div>
